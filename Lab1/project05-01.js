4968978249e4fbf37dc01a58f491896bad4bd906
seungjunekim-dev <seungjunekimk@gmail.com>
2026-02-19 16:37:39 -0500

    Update project05-01.js

--- Lab1/project05-01.js
+++ Lab1/project05-01.js
@@ -4,15 +4,15 @@
       Project 05-01
 
       Project to present an online quiz with a countdown clock
-      Author: 
-      Date:   
+      Author: Seungjune Kim
+      Date:   02/19/2026
 
       Filename: project05-01.js
 */
 
 // Constants to set the time given for the quiz in seconds
 // and the correct answers to each quiz question
-const quizTime = 20;
+const quizTime = 90;
 const correctAnswers = ["10", "4", "-6", "5", "-7"];
 
 // Elements in the quiz page
@@ -26,7 +26,50 @@ let timeLeft = quizTime;
 
 // Declare the ID for timed commands
 // and the node list for questions
+let timeID;
+let questionList = document.querySelectorAll("div#quiz input");
 
+// Add onclick event handler to start the quiz
+startQuiz.onclick = function() {
+   overlay.className = "showquiz";
+   timeID = setInterval(countdown, 1000);
+};
+
+// Function to update the quiz clock
+function countdown() {
+   if (timeLeft === 0) {
+      clearInterval(timeID);
+      let totalCorrect = checkAnswers();
+      
+      if (totalCorrect === correctAnswers.length) {
+         window.alert("Congratulations on getting 100%!");
+      } else {
+         let incorrectAnswers = correctAnswers.length - totalCorrect;
+         window.alert("You have " + incorrectAnswers + " incorrect answers out of " + correctAnswers.length + " questions.");
+         timeLeft = quizTime;
+         quizClock.value = timeLeft;
+         overlay.className = "hidequiz";
+      }
+   } else {
+      timeLeft--;
+      quizClock.value = timeLeft;
+   }
+}
+
+/*------------- Function to check the student answers ----------------*/
+function checkAnswers() {
+   let correctCount = 0;
+   
+   for (let i = 0; i < questionList.length; i++) {
+      if (questionList[i].value === correctAnswers[i]) {
+         correctCount++;
+         questionList[i].className = "";
+      } else {
+         questionList[i].className = "wronganswer";
+      }      
+   }
+   return correctCount;
+}
 
 
 
@@ -61,4 +104,5 @@ function checkAnswers() {
    }
    return correctCount;
 }
+
 
